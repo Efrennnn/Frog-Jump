e2795de250ae10e5d9e550bc8f704af7031924ac
Efren Christofer <111559192+Efrennnn@users.noreply.github.com>
2024-10-14 00:39:04 +0700

    FrogJump

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -10,15 +10,42 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
+    private Collider2D collider;
     private bool isGrounded;
+    private bool doubleJump;
+    private bool isFalling;
+
+    private void TriggerJump()
+    {
+        animator.SetTrigger("Jump"); 
+    }
+
+    private void TriggerDoubleJump()
+    {
+        animator.SetTrigger("DoubleJump"); 
+        animator.ResetTrigger("Jump");     
+    }
+
+    private void PlayFall()
+    {
+        if (!isGrounded && !animator.GetCurrentAnimatorStateInfo(0).IsName("DoubleJump"))
+        {
+            animator.SetBool("isFalling", true); 
+        }
+    }
+
+    private void StopFall()
+    {
+        animator.SetBool("isFalling", false); 
+    }
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Flip sprite based on horizontal input
     private void SpriteFlip(float horizontalInput)
     {
         if (horizontalInput < 0)
@@ -33,32 +60,57 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
-
         float horizontalInput = Input.GetAxis("Horizontal");
-        animator.SetFloat("speed", Mathf.Abs(horizontalInput));
-
+        animator.SetFloat("speed", Mathf.Abs(horizontalInput));        
+        
+        
         transform.Translate(new Vector3(horizontalInput * speed * Time.deltaTime, 0f, 0f));
         SpriteFlip(horizontalInput);
+    }
 
+    void Update()
+    {
+        
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            doubleJump = true; 
+            TriggerJump(); 
+        }
+        else if (doubleJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0f); 
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            doubleJump = false; 
+            TriggerDoubleJump(); 
+        }
+
+        
+        if (!isGrounded && rb.velocity.y < 0 && !animator.GetCurrentAnimatorStateInfo(0).IsName("DoubleJump"))
+        {
+            PlayFall(); 
+        }
+
+       
+        if (isGrounded)
+        {
+            StopFall(); 
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    public void OnCollisionEnter2D(Collision2D collider)
     {
-
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collider.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+            doubleJump = false; 
+            StopFall(); 
         }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
+    public void OnCollisionExit2D(Collision2D collider)
     {
-
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collider.gameObject.CompareTag("Ground"))
         {
             isGrounded = false;
         }
